4b2000004c067a571386c7dd2c4d73c56b56f4dc
3zAcr2S <leozhang1993@hotmail.com>
2017-03-24 20:12:09 -0400

    Add files via upload

--- AP_Motors/AP_MotorsCoax.cpp
+++ AP_Motors/AP_MotorsCoax.cpp
@@ -3,12 +3,10 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,62 +20,42 @@
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Math/AP_Math.h>
 #include "AP_MotorsCoax.h"
+#include <GCS_MAVLink/GCS.h>
 
 extern const AP_HAL::HAL& hal;
 
 
-const AP_Param::GroupInfo AP_MotorsCoax::var_info[] = {
-    // variables from parent vehicle
-    AP_NESTEDGROUPINFO(AP_MotorsMulticopter, 0),
-
-    // parameters 1 ~ 29 were reserved for tradheli
-    // parameters 30 ~ 39 reserved for tricopter
-    // parameters 40 ~ 49 for single copter and coax copter (these have identical parameter files)
-
-    // 40 was ROLL_SV_REV
-    // 41 was PITCH_SV_REV
-    // 42 was YAW_SV_REV
-
-	// @Param: SV_SPEED
-    // @DisplayName: Servo speed 
-    // @Description: Servo update speed
-    // @Units: Hz
-    AP_GROUPINFO("SV_SPEED", 43, AP_MotorsCoax, _servo_speed, AP_MOTORS_SINGLE_SPEED_DIGITAL_SERVOS),
-
-    // @Group: SV1_
-    // @Path: ../RC_Channel/RC_Channel.cpp
-    AP_SUBGROUPINFO(_servo1, "SV1_", 44, AP_MotorsCoax, RC_Channel),
-    // @Group: SV2_
-    // @Path: ../RC_Channel/RC_Channel.cpp
-    AP_SUBGROUPINFO(_servo2, "SV2_", 45, AP_MotorsCoax, RC_Channel),
-    // @Group: SV3_
-    // @Path: ../RC_Channel/RC_Channel.cpp
-    AP_SUBGROUPINFO(_servo3, "SV3_", 46, AP_MotorsCoax, RC_Channel),
-    // @Group: SV4_
-    // @Path: ../RC_Channel/RC_Channel.cpp
-    AP_SUBGROUPINFO(_servo4, "SV4_", 47, AP_MotorsCoax, RC_Channel),
-
-    AP_GROUPEND
-};
 // init
-void AP_MotorsCoax::Init()
+void AP_MotorsCoax::init(motor_frame_class frame_class, motor_frame_type frame_type)
 {
-    // set update rate for the 3 motors (but not the servo on channel 7)
-    set_update_rate(_speed_hz);
-
+    _servo1 = SRV_Channels::get_channel_for(SRV_Channel::k_motor1, CH_1);
+    _servo2 = SRV_Channels::get_channel_for(SRV_Channel::k_motor2, CH_2);
+    _servo3 = SRV_Channels::get_channel_for(SRV_Channel::k_motor3, CH_3);
+    _servo4 = SRV_Channels::get_channel_for(SRV_Channel::k_motor4, CH_4);
+    if (!_servo1 || !_servo2 || !_servo3 || !_servo4) {
+        GCS_MAVLINK::send_statustext_all(MAV_SEVERITY_ERROR, "MotorsCoax: unable to setup output channels");
+        // don't set initialised_ok
+        return;
+    }
+    
     // set the motor_enabled flag so that the main ESC can be calibrated like other frame types
     motor_enabled[AP_MOTORS_MOT_5] = true;
     motor_enabled[AP_MOTORS_MOT_6] = true;
 
     // we set four servos to angle
-    _servo1.set_type(RC_CHANNEL_TYPE_ANGLE);
-    _servo2.set_type(RC_CHANNEL_TYPE_ANGLE);
-    _servo3.set_type(RC_CHANNEL_TYPE_ANGLE);
-    _servo4.set_type(RC_CHANNEL_TYPE_ANGLE);
-    _servo1.set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
-    _servo2.set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
-    _servo3.set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
-    _servo4.set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
+    _servo1->set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
+    _servo2->set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
+    _servo3->set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
+    _servo4->set_angle(AP_MOTORS_COAX_SERVO_INPUT_RANGE);
+
+    // record successful initialisation if what we setup was the desired frame_class
+    _flags.initialised_ok = (frame_class == MOTOR_FRAME_COAX);
+}
+
+// set frame class (i.e. quad, hexa, heli) and type (i.e. x, plus)
+void AP_MotorsCoax::set_frame_class_and_type(motor_frame_class frame_class, motor_frame_type frame_type)
+{
+    _flags.initialised_ok = (frame_class == MOTOR_FRAME_COAX);
 }
 
 // set update rate to motors - a value in hertz
@@ -117,14 +95,32 @@ void AP_MotorsCoax::enable()
 
 void AP_MotorsCoax::output_to_motors()
 {
+ 	float ch7_in = read_ch();
+/*	float ch1_in = read_ch1();
+	float ch2_in = read_ch2();
+	float ch3_in = read_ch3();
+	float ch4_in = read_ch4();
+	float ch5_in = read_ch5();
+	
+	hal.console->printf("Channel 1 %f\n", ch1_in);
+	hal.console->printf("Channel 2 %f\n", ch2_in);
+	hal.console->printf("Channel 3 %f\n", ch3_in);
+	hal.console->printf("Channel 4 %f\n", ch4_in);
+	hal.console->printf("Channel 5 %f\n", ch5_in);
+	hal.console->printf("Channel 6 %f\n", ch7_in); */
+	//hal.console->printf("Channel 7 min: %f\n", RC_Channel::get_radio_min(7));
+	//hal.console->printf("Channel 7 max: %f\n", RC_Channel::get_radio_max(7));
+	
+	
+	
     switch (_spool_mode) {
         case SHUT_DOWN:
             // sends minimum values out to the motors
             hal.rcout->cork();
-            rc_write(AP_MOTORS_MOT_1, calc_pwm_output_1to1(+ _pitch_radio_passthrough + _yaw_radio_passthrough , _servo1));
-            rc_write(AP_MOTORS_MOT_2, calc_pwm_output_1to1(- _pitch_radio_passthrough + _yaw_radio_passthrough , _servo2));
-            rc_write(AP_MOTORS_MOT_3, calc_pwm_output_1to1(- _pitch_radio_passthrough - _roll_radio_passthrough, _servo3));
-            rc_write(AP_MOTORS_MOT_4, calc_pwm_output_1to1(+ _pitch_radio_passthrough - _roll_radio_passthrough, _servo4));
+            rc_write(AP_MOTORS_MOT_1, calc_pwm_output_1to1(_pitch_radio_passthrough + _yaw_radio_passthrough*0.1 - ch7_in, _servo1));
+            rc_write(AP_MOTORS_MOT_2, calc_pwm_output_1to1(-_pitch_radio_passthrough + _yaw_radio_passthrough*0.1 + ch7_in, _servo2));
+            rc_write(AP_MOTORS_MOT_3, calc_pwm_output_1to1(-_pitch_radio_passthrough + _roll_radio_passthrough, _servo3));
+            rc_write(AP_MOTORS_MOT_4, calc_pwm_output_1to1(_pitch_radio_passthrough + _roll_radio_passthrough , _servo4));
             rc_write(AP_MOTORS_MOT_5, get_pwm_output_min());
             rc_write(AP_MOTORS_MOT_6, get_pwm_output_min());
             hal.rcout->push();
@@ -183,14 +179,14 @@ float thrust_out; //
 float rp_scale = 1.0f; // this is used to scale the roll, pitch and yaw to fit within the motor limits
 float actuator_allowed = 0.0f; // amount of yaw we can fit in
 float actuator[NUM_ACTUATORS]; // combined roll, pitch and yaw thrusts for each actuator
-float lateral_thrust;
+float forward_thrust;
 
 // apply voltage and air pressure compensation
 roll_thrust = _roll_in * get_compensation_gain();
 pitch_thrust = _pitch_in * get_compensation_gain();
 yaw_thrust = _yaw_in * get_compensation_gain();
 throttle_thrust = get_throttle() * get_compensation_gain();
-lateral_thrust = _lateral_in;
+forward_thrust = read_ch();
 
 // sanity check throttle is above zero and below current limited throttle
 if (throttle_thrust <= 0.0f) {
@@ -235,19 +231,30 @@ thr_adj = MIN(thrust_min_rpy, _throttle_avg_max) - _throttle_avg_max;
 
 // calculate the throttle setting for the lift fan
 thrust_out = _throttle_avg_max + thr_adj;
-_thrust_yt_ccw = thrust_out - roll_thrust; // Right rotor motor
-_thrust_yt_cw = thrust_out + roll_thrust; // Left rotor motor
+_thrust_yt_ccw = thrust_out - roll_thrust* (1 - forward_thrust) - forward_thrust * yaw_thrust; // Right rotor motor
+_thrust_yt_cw  = thrust_out + roll_thrust* (1 - forward_thrust) + forward_thrust * yaw_thrust; // Left rotor motor
 // Adding compenstation for pitch when we have a better understading of the rotors
 // in these would improve accuracy but skip until later
 // Main rotor servos
-actuator[0] = rp_scale*pitch_thrust + yaw_thrust + lateral_thrust; // Right
-actuator[1] = - rp_scale*pitch_thrust + yaw_thrust + lateral_thrust; // Left
+actuator[0] = 		(1 - forward_thrust) * rp_scale * pitch_thrust + (1 - forward_thrust) * yaw_thrust - forward_thrust; // Right 
+//actuator[1] =  (	(1 - forward_thrust) * rp_scale * pitch_thrust - (1 - forward_thrust) * yaw_thrust - forward_thrust); // Left * Use this if your servo is reversed 
+actuator[1] = - 	(1 - forward_thrust) * rp_scale * pitch_thrust + (1 - forward_thrust) * yaw_thrust + forward_thrust; // Left 
+
+
 // Flap servos
-actuator[2] = -(pitch_thrust + roll_thrust)/2; //Right
-actuator[3] = (pitch_thrust - roll_thrust)/2; //Left
+actuator[2] = -	(5.0f*pitch_thrust - roll_thrust); //Right
+actuator[3] = 	(5.0f*pitch_thrust + roll_thrust); //Left
 
 //_rpm_R = throttle_thrust - roll_thrust; // Right rotor motor
 //_rpm_L = throttle_thrust + roll_thrust; // Left rotor motor
+
+
+// constrain the servo motors so that they can only 
+// pivot around 30 degree about the netrual point, which set by forward_thrust.
+for (uint8_t i=0; i<2; i++) {
+_actuator_out[i] = constrain_float(actuator[i], forward_thrust - 0.3f, forward_thrust + 0.3f);  
+}
+
 
 for (uint8_t i=0; i<NUM_ACTUATORS; i++) {
 _actuator_out[i] = constrain_float(actuator[i], -1.0f, 1.0f);
@@ -267,11 +274,11 @@ void AP_MotorsCoax::output_test(uint8_t motor_seq, int16_t pwm)
     // output to motors and servos
     switch (motor_seq) {
         case 1:
-            // flap servo 1
+            // rotor servo 1
             rc_write(AP_MOTORS_MOT_1, pwm);
             break;
         case 2:
-            // flap servo 2
+            // rotor servo 2
             rc_write(AP_MOTORS_MOT_2, pwm);
             break;
         case 3:
@@ -294,4 +301,11 @@ void AP_MotorsCoax::output_test(uint8_t motor_seq, int16_t pwm)
             // do nothing
             break;
     }
+}
+
+float AP_MotorsCoax::read_ch()
+{
+    int16_t forward = hal.rcin->read(6);
+	float forward_f = (float(forward) - 1099.0f)/802.0f;
+	return forward_f;
 }
--- AP_Motors/AP_MotorsCoax.h
+++ AP_Motors/AP_MotorsCoax.h
@@ -4,7 +4,7 @@
 
 #include <AP_Common/AP_Common.h>
 #include <AP_Math/AP_Math.h>        // ArduPilot Mega Vector/Matrix math Library
-#include <RC_Channel/RC_Channel.h>     // RC Channel Library
+#include <SRV_Channel/SRV_Channel.h>
 #include "AP_MotorsMulticopter.h"
 
 // feedback direction
@@ -16,7 +16,7 @@
 #define AP_MOTORS_SINGLE_SPEED_DIGITAL_SERVOS 250 // update rate for digital servos
 #define AP_MOTORS_SINGLE_SPEED_ANALOG_SERVOS 50  // update rate for analog servos
 
-#define AP_MOTORS_COAX_SERVO_INPUT_RANGE    4500    // roll or pitch input of -4500 will cause servos to their minimum (i.e. radio_min), +4500 will move them to their maximum (i.e. radio_max)
+#define AP_MOTORS_COAX_SERVO_INPUT_RANGE    9000    // roll or pitch input of -4500 will cause servos to their minimum (i.e. radio_min), +4500 will move them to their maximum (i.e. radio_max)
 
 /// @class      AP_MotorsSingle
 class AP_MotorsCoax : public AP_MotorsMulticopter {
@@ -24,14 +24,15 @@ public:
 
     /// Constructor
     AP_MotorsCoax(uint16_t loop_rate, uint16_t speed_hz = AP_MOTORS_SPEED_DEFAULT) :
-        AP_MotorsMulticopter(loop_rate, speed_hz),
-        _servo1(CH_NONE), _servo2(CH_NONE), _servo3(CH_NONE), _servo4(CH_NONE)
+        AP_MotorsMulticopter(loop_rate, speed_hz)
     {
-        AP_Param::setup_object_defaults(this, var_info);
     };
 
     // init
-    virtual void        Init();
+    void                init(motor_frame_class frame_class, motor_frame_type frame_type);
+
+    // set frame class (i.e. quad, hexa, heli) and type (i.e. x, plus)
+    void                set_frame_class_and_type(motor_frame_class frame_class, motor_frame_type frame_type);
 
     // set update rate to motors - a value in hertz
     void                set_update_rate( uint16_t speed_hz );
@@ -50,24 +51,24 @@ public:
     // get_motor_mask - returns a bitmask of which outputs are being used for motors or servos (1 means being used)
     //  this can be used to ensure other pwm outputs (i.e. for servos) do not conflict
     virtual uint16_t    get_motor_mask();
-
-    // var_info for holding Parameter information
-    static const struct AP_Param::GroupInfo var_info[];
+	//#Let's see if works
+	float               read_ch();
+	float               read_ch1();
+	float               read_ch2();
+	float               read_ch3();
+	float               read_ch4();
+	float               read_ch5();
+	
 
 protected:
     // output - sends commands to the motors
     void                output_armed_stabilizing();
 
-    // servo speed
-    AP_Int16            _servo_speed;
-
-    // Allow the use of a 4 servo output to make it easy to test coax and single using same airframe
-    RC_Channel          _servo1;
-    RC_Channel          _servo2;
-    RC_Channel          _servo3;
-    RC_Channel          _servo4;
-
     float               _actuator_out[NUM_ACTUATORS]; // combined roll, pitch, yaw and throttle outputs to motors in 0~1 range
     float               _thrust_yt_ccw;
     float               _thrust_yt_cw;
+    SRV_Channel         *_servo1;
+    SRV_Channel         *_servo2;
+    SRV_Channel         *_servo3;
+    SRV_Channel         *_servo4;
 };
